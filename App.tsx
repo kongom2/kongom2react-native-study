8a7afbd6614eccb6effc354517a5d4081a457a9e
kongom2@gmail.com <kongom2@gmail.com>
2022-03-23 19:36:40 +0900

    feat: init

---
+++ App.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable react-native/no-inline-styles */
+import * as React from 'react';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {Pressable, Text, TouchableHighlight, View} from 'react-native';
+import {useCallback} from 'react';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
+
+function HomeScreen({navigation}: HomeScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
+
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <View
+        style={{
+          flex: 5,
+          backgroundColor: 'yellow',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Pressable
+          onPress={onClick}
+          style={{
+            paddingTop: 20,
+            paddingBottom: 20,
+            paddingRight: 40,
+            paddingLeft: 40,
+            backgroundColor: 'blue',
+            borderRadius: 20,
+          }}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>Home Screen</Text>
+        </Pressable>
+      </View>
+      <View style={{flex: 2, backgroundColor: 'orange'}}>
+        <Text>Second</Text>
+      </View>
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}: DetailsScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>Details Screen</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: '홈화면'}}
+        />
+        <Stack.Screen name="Details">
+          {props => <DetailsScreen {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
